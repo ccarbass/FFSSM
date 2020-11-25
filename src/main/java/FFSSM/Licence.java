875932beaf00dc7d164e380e7f96c046dccb8ec1
Camille <ca.carbasse@orange.fr>
2020-11-25 09:47:48 +0100

    modification méthode est valid dans licence

--- src/main/java/FFSSM/Licence.java
+++ src/main/java/FFSSM/Licence.java
@@ -52,8 +52,13 @@ public class Licence {
      * @return vrai si valide à la date d
      **/
     public boolean estValide(LocalDate d) {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        LocalDate delivranceplus1= this.delivrance.plusYears(1);
+        if(d.isBefore(delivranceplus1) ){
+            return true;
+         }
+         else{
+             return false;
+         }
     }
 
 }
